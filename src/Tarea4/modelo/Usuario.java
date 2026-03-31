63a586d10c93b20795d40c15005bad871278afa9
Tomas <tomasdanielbritovargaz@gmail.com>
2026-03-31 13:41:56 -0400

    Persona y Usuario

--- src/Tarea4/modelo/Usuario.java
+++ src/Tarea4/modelo/Usuario.java
@@ -1,5 +1,62 @@
 package Tarea4.modelo;
 
-public class Usuario {
+public class Usuario extends Persona {
 
+    private int id;
+    private String nombreUsuario;
+    private String contrasena;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String nombreUsuario, String contrasena, String nombre, String apellido, String telefono, String correo) {
+        super(nombre, apellido, telefono, correo);
+        this.id = id;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasena = contrasena;
+    }
+
+    public Usuario(String nombreUsuario, String contrasena, String nombre, String apellido, String telefono, String correo) {
+        super(nombre, apellido, telefono, correo);
+        this.nombreUsuario = nombreUsuario;
+        this.contrasena = contrasena;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    @Override
+    public String mostrarInformacion() {
+        return "Usuario: " + nombreUsuario +
+               ", Nombre: " + getNombre() +
+               ", Apellido: " + getApellido() +
+               ", Telefono: " + getTelefono() +
+               ", Correo: " + getCorreo();
+    }
+
+    @Override
+    public String toString() {
+        return getNombre() + " " + getApellido();
+    }
 }
