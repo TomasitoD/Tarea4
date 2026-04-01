95f0a601b24549e0af312c1ccbca4128b33e1c81
Tomas <tomasdanielbritovargaz@gmail.com>
2026-03-31 21:22:02 -0400

    Main comentado

--- src/Tarea4/Main.java
+++ src/Tarea4/Main.java
@@ -2,9 +2,11 @@ package Tarea4;
 
 import Tarea4.vistas.VentanaLogin;
 
+// Clase principal que inicia la aplicacion mostrando la ventana de login.
 public class Main {
 
     public static void main(String[] args) {
+        // Se abre la primera ventana del sistema.
         new VentanaLogin();
     }
 }
